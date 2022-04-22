a5b24e8ff532b01b7e49afaa801470705c9e6658
yoshi <yoshi4868686@gmail.com>
2022-04-22 21:19:38 +0800

    Add docusaurus.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Yoshi\'s website',
   tagline: 'Dinosaurs are cool',
   url: 'https://ycc789741ycc.github.io',
-  baseUrl: '/yoshi-website/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
